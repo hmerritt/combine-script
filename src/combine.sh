5eea648d6bf8d1f6c1d6bd95745a4537024c119a
Harry Merritt <harry@merritt.es>
2021-03-15 23:42:18 +0000

    Add function to print all functions within current script

--- src/combine.sh
+++ src/combine.sh
@@ -3,6 +3,16 @@
 __COMBINESCRIPT__VERSION=0.0.1
 
 
+# Print a list of all functions
+# within the current script
+function __combinescript__print_functions
+{
+	echo "Functions"
+    grep "^function" $0
+    grep "^()" $0
+}
+
+
 # Command,  run
 # > Run function via CLI
 if [ "${1}" = "run" ]; then
