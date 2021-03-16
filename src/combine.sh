8797f4f2e69c13ab943d13e0770c0b43f0eb195a
Harry Merritt <harry@merritt.es>
2021-03-16 17:29:50 +0000

    Add a few helper functions
    - Removed injection helper functions for the time being

--- src/combine.sh
+++ src/combine.sh
@@ -1,75 +1,51 @@
 #!/bin/bash
 
-__COMBINESCRIPT__VERSION=0.1.0
+#
+# Combine.sh
+# https://github.com/hmerritt/combine-script
+#
+# Usage:
+# ./combine.sh <path-to-scripts-directory> <output-script-name>
+# ./combine.sh ./scripts script.sh
+#
 
 
-# Print a list of all functions
-# within the current script
-function __combinescript__print_functions
+# Use a fallback value if initial value does not exist
+# - Usage: value=$(fallback $1 "fallback")
+# - $1: initial value
+# - $2: fallback value
+function fallback
 {
-	echo "Functions"
-    grep "^function" $0
-    grep "^()" $0
+	local value=$1
+
+	# Check for NULL/empty value
+	if [ ! -n "${value}" ]; then
+		local value=$2
+	fi
+
+	echo "${value}"
 }
 
 
-# Print a list of script commands
-function __combinescript__print_commands
+# Print script help
+function print_help
 {
-    echo "Commands"
-    echo "  * run     |  run a function"
-    echo "  * help    |  print script help"
-    echo "  * repl    |  shell to run multiple commands (not for programmatic use)"
+    echo "combine <path-to-scripts-directory> <output-script-name>"
+    echo "combine ./scripts script.sh"
 }
 
 
-# Print combine-script help
-function __combinescript__print_help
+# Remove file if it exists
+function delete_file
 {
-    echo "Help:"
-    echo
-    __combinescript__print_commands
-    echo
-    echo
-    __combinescript__print_functions
-}
-
-
-# Print combine-script help message
-# if no arguments have been passed
-if [ "${1}" = "" ] || [ "${1}" = "help" ] || [ "${1}" = "h" ]; then
-  __combinescript__print_help
-  exit 1
-fi
-
-
-# Command,  run
-# > Run function via CLI
-if [ "${1}" = "run" ]; then
-    if [ "${2}" = "" ]; then
-        echo "no function passed"
-        echo "run <function>"
-        echo
-    else
-        "${@: 2}"
+    if [ -f "${1}" ]; then
+        rm "${1}"
     fi
-fi
+}
 
 
-# Command,  repl
-# > Interactive REPL
-if [ "${1}" = "repl" ]; then
-  __combinescript__print_help
-
-  # Init repl
-  while true ; do
-    while IFS="" read -r -e -d $'\n' -p ':$ ' options; do
-  	  if [ "$options" = "quit" ]; then
-	    exit 0
-	  else
-	    ${options}
-	    echo
-	  fi
-	done
-  done
+# Print script help message
+if [ "${1}" = "help" ] || [ "${1}" = "h" ]; then
+  print_help
+  exit 1
 fi
