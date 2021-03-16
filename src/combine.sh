2fce6a59e56b355c4d9a0e699fda9ee29afb1fe4
Harry Merritt <harry@merritt.es>
2021-03-16 17:40:14 +0000

    Inject combine helper functions into final output file

--- src/combine.sh
+++ src/combine.sh
@@ -124,3 +124,92 @@ EOF
 
 # Append bundle to $SCRIPT_OUTPUT_PATH
 cat "${BUNDLE_PATH}" >> "${SCRIPT_OUTPUT_PATH}"
+
+
+# Include helper function within $SCRIPT_OUTPUT_PATH
+# Default: yes
+if [ "${BUNDLE_HELPER_FUNCTIONS}" = "yes" ]; then
+    echo
+    echo -e "Injecting combine helper functions"
+
+    cat <<EOF >> ${SCRIPT_OUTPUT_PATH}
+
+
+#
+# Combine.sh Helper Functions
+#
+
+# Print a list of all functions
+# within the current script
+function __combinescript__print_functions
+{
+	echo "Functions"
+    grep "^function" \$0
+    grep "^()" \$0
+}
+
+
+# Print a list of script commands
+function __combinescript__print_commands
+{
+    echo "Commands"
+    echo "  * run     |  run a function"
+    echo "  * help    |  print script help"
+    echo "  * repl    |  shell to run multiple commands (not for programmatic use)"
+}
+
+
+# Print combine-script help
+function __combinescript__print_help
+{
+    echo "Help:"
+    echo
+    __combinescript__print_commands
+    echo
+    echo
+    __combinescript__print_functions
+}
+
+
+# Print combine-script help message
+# if no arguments have been passed
+if [ "\${1}" = "" ] || [ "\${1}" = "help" ] || [ "\${1}" = "h" ]; then
+  __combinescript__print_help
+  exit 1
+fi
+
+
+# Command,  run
+# > Run function via CLI
+if [ "\${1}" = "run" ]; then
+    if [ "\${2}" = "" ]; then
+        echo -e "\e[31mNo function passed\033[0m"
+        echo "run <function>"
+        echo
+        exit 0
+    else
+        "\${@: 2}"
+    fi
+fi
+
+
+# Command,  repl
+# > Interactive REPL
+if [ "\${1}" = "repl" ]; then
+  __combinescript__print_help
+
+  # Init repl
+  while true ; do
+    while IFS="" read -r -e -d $'\n' -p ':$ ' options; do
+  	  if [ "\$options" = "quit" ]; then
+	    exit 0
+	  else
+	    \${options}
+	    echo
+	  fi
+	done
+  done
+fi
+
+EOF
+fi
